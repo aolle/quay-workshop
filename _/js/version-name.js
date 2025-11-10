9c1b444630bafde0cc61a187661ab165653db20f
aolle <angel@olleb.com>
2025-11-10 11:36:36 +0000

    Deploying to gh-pages from @ aolle/quay-workshop@0af3aa8c388945579fafa97016743af18ee310cd 🚀

--- _/js/version-name.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toolbar = document.querySelector('.toolbar');
-  const version = toolbar?.dataset.quayVersion;
-  const toggle = document.querySelector('.version-menu-toggle');
-  
-  if (toggle && version && toggle.textContent.trim() === 'main') {
-    toggle.textContent = version;
-  }
-});
