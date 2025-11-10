161dacd6259f513addc0663fbf93d2e65b0dacf4
Àngel Ollé Blázquez <angel@olleb.com>
2025-11-10 12:50:58 +0100

    Fix version format

--- supplemental-ui/js/version-name.js
+++ supplemental-ui/js/version-name.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const version = '3.15';
+  const version = 'v3.15';
   const toggle = document.querySelector('.version-menu-toggle');
 
   if (toggle && version && toggle.textContent.trim() === 'main') {
